b5382c72d1d212d3ae377bd3a9e7518b587f5dcc
fjcorrales <danielcorralesfalco@gmail.com>
2024-03-06 22:41:57 +0100

    Renamed files

---
+++ notes/9 - for_loops.py
@@ -0,0 +1,32 @@
+###el bucle for itera sobre items de una coleccion, se usa sobretodo con strings
+# es decir, el bucle for en python se ejecuta tantas veces como elementos tenga la coleccion que se usa como variable
+# de bucle###
+
+for item in 'Python':
+    print(item)
+
+print('\n')
+
+### En python podemos definir una lista con []###
+nombres = ['Juan', 'Daniel', 'Maria']
+for item in nombres:
+    print(item)
+
+print('\n')
+
+###Tambien se puede utilizar con numeros
+# en estos casos si queremos que itere sobre un amplio rango de numeros no hace falta escribir todos
+# tenemos a nuestra disposicion la funcion in range###
+
+for item in range(10):
+    print(item)
+
+print('\n')
+
+for item in range(5, 10):   #para poner un intervalo
+    print(item)
+
+print('\n')
+
+for item in range(5, 10, 2): #para poner un intervalo y que de saltos
+    print(item)
