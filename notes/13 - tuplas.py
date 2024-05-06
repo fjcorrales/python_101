a11e60ffe0e953111b59b2f33e5ea11368ecb38f
fjcorrales <danielcorralesfalco@gmail.com>
2024-05-06 23:01:11 +0200

    tuplas

---
+++ notes/13 - tuplas.py
@@ -0,0 +1,10 @@
+#Las tuplas son como listas pero estas son inmutables, no podemos realiazr ninguna modificacion sobre estas
+
+numeros = (1, 2, 3)
+
+
+#Podemos acceder los elementos como en las listas
+print(numeros[0])
+
+#Pero no podemos modificar nada
+#numeros[0] = 10
