210f2ef6d4c190d23f380bca1576e96c96775a35
fjcorrales <danielcorralesfalco@gmail.com>
2024-05-11 17:37:05 +0200

    modulos

---
+++ notes/18 - modulos.py
@@ -0,0 +1,14 @@
+#un modulo no es mas que un archivo con codigo python que importamos a otro fichero para poder usar sus funciones o clases
+#en este caso hemos creado un fichero llamado conversores en el que tenemos dos funciones, vamos a importarlo y a hacer
+#uso de las mismas. Crear un modulo no es mas que crear un fichero, llenarlo de funciones e importatlo donde sea necesario
+
+#para importar hacemos uso de la sentencia "import" esto hace que la clase que importamos se convierta en un objeto
+import conversores
+
+print(conversores.kg_to_lbs(80))
+print(conversores.lbs_to_kg(177))
+
+#no existe solo esta forma de realizar importaciones de modulos
+#haciendo de uso de la sentencia "from" podemos importar una unica funcion a nuestro fichero
+from conversores import kg_to_lbs
+print(kg_to_lbs(100))
---
+++ notes/conversores.py
@@ -0,0 +1,6 @@
+def lbs_to_kg(peso):
+    return peso * 0.45
+
+
+def kg_to_lbs(peso):
+    return peso / 0.45
