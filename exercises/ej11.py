86c4f702ca28aced482511122241fddd24fc3b64
fjcorrales <danielcorralesfalco@gmail.com>
2024-05-11 18:18:59 +0200

    valores aleatorios y libreria estandar

--- exercises/ej11.py
+++ exercises/ej11.py
@@ -1,8 +1,9 @@
 #Definir una clase 'Dado' que tenga un metodo llamado 'tirada' y que este devuelva una tupla de valores
 
 import random
-class Dado:
 
+
+class Dado:
     def tirada(self):
         valor1 = random.randint(1, 6)
         valor2 = random.randint(1, 6)
