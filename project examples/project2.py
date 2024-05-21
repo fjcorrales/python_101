993de9c597a484717faeb18dad0ca80996e2f9b1
fjcorrales <danielcorralesfalco@gmail.com>
2024-05-21 22:45:19 +0200

    Machine learning con python intro

--- project examples/project2.py
+++ project examples/project2.py
@@ -1,1 +1,22 @@
 #este proyecto tratara de implementar machine learning con python
+#para poder implementar esta funcionalidad con python hemos de seguir un esquema
+#   importar datos (normalmente como un .csv)
+#   limpiar los datos (eliminar duplicados, incompletos, irrelevantes... etc)
+#   dividir estos datos en grupos de entrenamiento y otros de prueba
+#   crear un modelo (implementar un algoritmo)
+#   entrenar el modelo (pasarle el grupo de entrenamiento)
+#   hacer predicciones con el modelo ya entrenado
+#   evaluar los resultados y mejorar el proceso (cambiando el algoritmo, modificando parametros... etc)
+
+#haremos uso de las librerias: numpy, pandas, matplotlib, scikit-learn(libreria de machine learning)
+#dado que pycharm no cuenta con las herramientas necesarias para hacer uso de machine learning de una forma comoda
+#y no se va a poder visualizar correctamente la informacion proporcionada por el algoritmo, este ejercicio se va a
+#realizar desde una plataforma online llamada 'jupyter', para hacer uso de esta hemos de instalar el software de ANACONDA
+#https://www.anaconda.com/download/success
+#https://jupyter.org
+
+#tras instalar anaconda hemos de ejecutar en nuestra consola de comandos o terminal 'jupyter notebook'
+#esto nos abrira una pestaña en el navegador que nos permitira navegar nuestro directorio, desde aqui
+#hemos de crear un python notebook en el directorio que queramos, esto nos permitira ejecutar codigo de pyton desde
+#el mismo navegador dentro de nuestra instancia de jupyter
+
